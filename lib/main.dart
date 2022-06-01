8ffbb12db2038160415e79fb284fbc57a48bd538
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-06-01 18:33:17 +0300

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:a_weather/pages/home/home_screen.dart';
+import 'package:a_weather/utils/bindings/home_binding.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomeScreen(),
+          binding: HomeBinding(),
+        )
+      ],
+    );
+  }
+}
